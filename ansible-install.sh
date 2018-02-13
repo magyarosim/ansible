4c9aa225836e337b97784bfe62d6598d1ceff5de
magyarosim <36436545+magyarosim@users.noreply.github.com>
2018-02-13 10:04:55 +0100

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -5,3 +5,4 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible
+sudo ansible-playbook playbook.yml -i hosts -e mysql_root_password=#password#
